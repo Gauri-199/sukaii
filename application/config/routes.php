78518abeb51d3d06c2db04ad70947b6c8983a10d
gbtechsupriya17 <82633479+gbtechsupriya17@users.noreply.github.com>
2022-10-17 13:39:43 +0530

    create package pages

--- application/config/routes.php
+++ application/config/routes.php
@@ -50,5 +50,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |		my-controller/my-method	-> my_controller/my_method
 */
 $route['default_controller'] = 'welcome';
+
+$route["covidRTPCRcheckup"]="OrdersController/covidRTPCRcheckup";
+$route["basicHeathjCheckup"]="OrdersController/basicHeathjCheckup";
+$route["completeHealthCheckup"]="OrdersController/completeHealthCheckup";
+$route["lenLenCheckUp"]="OrdersController/lenLenCheckUp";
+
+
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
